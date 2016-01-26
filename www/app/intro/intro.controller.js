2d5fb69235caba460554c7b3d5fa3f6eb7f70245
Robert Horlings <robert@isdat.nl>
2016-01-26 11:08:11 +0100

    Now redirecting to proper login page on 401

--- www/app/intro/intro.controller.js
+++ www/app/intro/intro.controller.js
@@ -2,12 +2,12 @@
 	angular.module('healthcafe.intro')
 		.controller('IntroController', IntroController );
 
-	IntroController.$inject = [ '$scope', '$ionicHistory' ];
+	IntroController.$inject = [ '$scope', '$ionicHistory', 'OAuth2' ];
 
-	function IntroController($scope, $ionicHistory) {
+	function IntroController($scope, $ionicHistory, OAuth2) {
 	  var vm = this;
     $scope.$on('event:auth-unauthorized', function(event, data){
-      $state.go( "login" );
+      OAuth2.login();
     });
 
     // Method to reset navigation and disable back on the next page
