a0c840eabff0c82bef36abc4db4d754bd33d3b25
Robert Horlings <robert@isdat.nl>
2016-02-12 14:40:12 +0100

    Added popup after sharing or import finished

--- www/app/share/share.controller.js
+++ www/app/share/share.controller.js
@@ -2,9 +2,9 @@
 	angular.module('healthcafe.sharing')
 		.controller('ShareController', ShareController );
 
-		ShareController.$inject = [ '$state', 'OAuth2', 'config', 'Share'];
+		ShareController.$inject = [ '$state', '$ionicPopup', 'OAuth2', 'config', 'Share'];
 
-		function ShareController($state, OAuth2, config, Share) {
+		function ShareController($state, $ionicPopup, OAuth2, config, Share) {
       var vm = this;
 
       var serviceKey = $state.params.service;
@@ -30,11 +30,43 @@
 
       // Method to import data
       vm.import = function() {
-        Share.pull(serviceKey, vm.datatypes);
+        Share.pull(serviceKey, vm.datatypes)
+          .then(function(e) {
+            var numDatapoints = e.reduce(function(previousSum, currentIds) { return previousSum + (currentIds ? currentIds.length : 0); }, 0);
+            $ionicPopup.alert({
+              "title": "Import succeeded",
+              "template": "" + numDatapoints + " data point(s) have been imported into your app."
+            }).then(function(res) {
+              // After popup, go to the timeline
+              $state.go("app.timeline");
+            });
+          }).catch(function(e) {
+            // If any of the request fails with a 401 error, the access token
+            // has most probably expired. Send the user to the login page again.
+            OAuth2.logoff(serviceKey);
+            $state.go("app.connect", { service: serviceKey });
+          });
       }
 
       vm.export = function() {
-        Share.push(serviceKey, vm.datatypes);
+        Share.push(serviceKey, vm.datatypes)
+          .then(function(e) {
+            var numDatapoints = e.reduce(function(previousSum, currentIds) { return previousSum + (currentIds ? currentIds.length : 0); }, 0);
+            $ionicPopup.alert({
+              "title": "Share succeeded",
+              "template": "" + numDatapoints + " data point(s) have been shared with " + vm.service.label + "."
+            }).then(function(res) {
+              // After popup, go to the timeline
+              $state.go("app.timeline");
+            });
+          }).catch(function(e) {
+            // If any of the request fails with a 401 error, the access token
+            // has most probably expired. Send the user to the login page again.
+            console.log(e);
+
+            //OAuth2.logoff(serviceKey);
+            //$state.go("app.connect", { service: serviceKey });
+          });
       }
 
 
