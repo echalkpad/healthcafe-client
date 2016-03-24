debb71449b5e4a4a827882568405b0ab9841b98f
Ferry Jagers <ferry.programming@iCloud.com>
2016-03-24 16:31:08 +0100

    Change nrc healthcafe data sharing configuration

--- www/app/app.config.js
+++ www/app/app.config.js
@@ -45,9 +45,12 @@
         oauth: {
           healthcafe: {
             urls: {
-              authorization: "http://localhost:8080/nrc/oauth/authorize",
-              redirect: "http://localhost:8100/app/login/oauth_callback.html?healthcafe",
-              dataPoints: "http://localhost:8080/nrc/api/openmhealth/v1/dataPoints"
+              // authorization: "http://localhost:8080/nrc/oauth/authorize",
+              // redirect: "http://localhost:8100/app/login/oauth_callback.html?healthcafe",
+              // dataPoints: "http://localhost:8080/nrc/api/openmhealth/v1/dataPoints"
+              authorization: "https://humanstudies.tno.nl/healthcafe/oauth/authorize",
+              redirect: "http://msb2.hex.tno.nl/app-test/login/oauth_callback.html?healthcafe",
+              dataPoints: "https://humanstudies.tno.nl/healthcafe/api/openmhealth/v1/dataPoints"
             },
 
             // NRC OAuth application key
